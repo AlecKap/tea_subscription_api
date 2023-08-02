54cfa2b03c6791a7542583a5332135e34bfb5fb4
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-08-01 23:24:33 -0600

    Add endpoint to cancel a customer subscription.

--- app/controllers/api/v0/customer_subscriptions_controller.rb
+++ app/controllers/api/v0/customer_subscriptions_controller.rb
@@ -4,9 +4,14 @@ class Api::V0::CustomerSubscriptionsController < ApplicationController
     render json: { message: 'Customer has been successfully subscribed.' }, status: :created
   end
 
+  def update
+    CustomerSubscription.find(params[:id]).update!(customer_subscription_params)
+    render json: { message: 'Customer subscription has been cancelled.' }, status: :ok
+  end
+
   private
 
   def customer_subscription_params
-    params.require(:customer_subscription).permit(:customer_id, :subscription_id)
+    params.require(:customer_subscription).permit(:customer_id, :subscription_id, :status)
   end
 end
---
+++ spec/requests/customer_subscriptions/update_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'update request' do
+  before :each do
+    test_data
+  end
+
+  describe 'happy path' do
+    it 'updates an existing customer subscription' do
+      id = CustomerSubscription.create!(customer_id: @customer2.id, subscription_id: @subscription4.id).id
+      previous_status = CustomerSubscription.last.status
+      customer_subscription_params = { status: 'cancelled' }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      patch "/api/v0/customer_subscriptions/#{id}", headers: headers, params: JSON.generate(customer_subscription: customer_subscription_params)
+      customer_subscription = CustomerSubscription.find(id)
+      status_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(customer_subscription.status).to_not eq(previous_status)
+      expect(customer_subscription.status).to eq('cancelled')
+      expect(status_response).to have_key(:message)
+      expect(status_response[:message]).to eq('Customer subscription has been cancelled.')
+    end
+  end
+end
