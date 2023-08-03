ead291a3d9fea3d33cd6ed8aabbbe794f4ad3683
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-08-03 09:59:50 -0600

    test: Update tests to be more robust.

--- spec/requests/subscriptions/delete_spec.rb
+++ spec/requests/subscriptions/delete_spec.rb
@@ -12,6 +12,9 @@ describe 'subscription destroy request' do
 
       expect { delete "/api/v0/subscriptions/#{id}" }.to change(Subscription, :count).by(-1)
       expect(response).to be_successful
+      status_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(status_response[:message]).to eq('Subscription has been successfully deleted.')
 
       expect { Subscription.find(id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
